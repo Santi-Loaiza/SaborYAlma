22a6f7414e9883b12f82a64de7aaf4d11bc6d653
Santiago Loaiza <santiagoloaizamadrid@gmail.com>
2024-10-27 20:20:14 -0500

    S1A JS  continuacion del menu con JavaScript

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,4 @@
-//Arrays de los platos
+//Arrays de las entradas
 const entradas = [
     {
         imagen: '../IMAGES/MENUSABORYALMA/TostadasNikkei.jpeg',
@@ -76,8 +76,166 @@ const entradas = [
     }
 ];
 
+// Array de los platos fuertes
+const platosFuertes = [
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/LomoSaborYAlma.jpg',
+      nombre: 'Lomo Sabor Y Alma',
+      descripcion: 'Lomo a término en parrilla bañado en salsa madeira, sobre un arroz cremoso a base de setas y queso azul, coronado con parmesano.',
+      precio: 55000,
+      ingredientesQuitar: ['Parmessano', 'Setas', 'Queso azul'],
+      ingredientesAñadir: [
+        { ingrediente: 'Espárragos salteados', precio: 6000 },
+        { ingrediente: 'Rúcula fresca', precio: 3000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/LomoSelvaEnRissotoDeAjíAmarillo.jpg',
+      nombre: 'Lomo Selva en Risotto de Ají Amarillo',
+      descripcion: 'Lomito jugoso en cama de risotto al ají amarillo diseñado con salsa de fresa y hierbabuena.',
+      precio: 53000,
+      ingredientesQuitar: ['Salsa de fresa', 'Hierbabuena'],
+      ingredientesAñadir: [
+        { ingrediente: 'Salsa de tamarindo', precio: 3000 },
+        { ingrediente: 'Tomates confitados', precio: 3500 },
+        { ingrediente: 'Albahaca fresca', precio: 2500 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/PolloTrufado.jpg',
+      nombre: 'Pollo Trufado',
+      descripcion: 'Julianas de pollo trufadas a la parrilla sobre una cama de pasta tipo Alfredo, coronadas con brotes.',
+      precio: 37000,
+      ingredientesQuitar: [],
+      ingredientesAñadir: [
+        { ingrediente: 'Queso parmessano', precio: 0 },
+        { ingrediente: 'Hongos portobello', precio: 4500 },
+        { ingrediente: 'Alcachofas asadas', precio: 5000 },
+        { ingrediente: 'Tomates Cherry confitados', precio: 3500 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/SalmonCremoso.jpg',
+      nombre: 'Salmón Cremoso',
+      descripcion: 'Filete de Salmón fresco a la parrilla, bañado en salsa cremosa blanca, pasta al pesto y crema de leche, coronado con quinua crocante.',
+      precio: 55000,
+      ingredientesQuitar: ['Quinua'],
+      ingredientesAñadir: [
+        { ingrediente: 'Pistachos triturado', precio: 2500 },
+        { ingrediente: 'Panko dorado', precio: 2000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/ArrozPeruano.jpeg',
+      nombre: 'Arroz Peruano de Mariscos',
+      descripcion: 'Arroz en diversidad de mariscos a base de ají amarillo y especias peruanas.',
+      precio: 60000,
+      ingredientesQuitar: ['Mejillones', 'Camarones'],
+      ingredientesAñadir: [
+        { ingrediente: 'Pulpo', precio: 14000 },
+        { ingrediente: 'Langostinos', precio: 9000 },
+        { ingrediente: 'Cangrejo', precio: 11000 },
+        { ingrediente: 'Almejas', precio: 8000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/PescadoTupac.jpg',
+      nombre: 'Pescado Tupac',
+      descripcion: 'Pescado blanco crocante con camarones bañados en salsa al leño a base de licor, champiñón y panceta, acompañados de papa rústica en cascos.',
+      precio: 46000,
+      ingredientesQuitar: ['Camarones', 'Champiñón', 'Panceta', 'Papas rusticas'],
+      ingredientesAñadir: [
+        { ingrediente: 'Alcachofas asadas', precio: 5000 },
+        { ingrediente: 'Pulpo a la parrilla', precio: 14000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/TallarinesAlEstiloOriental.jpg',
+      nombre: 'Tallarines al Estilo Oriental',
+      descripcion: 'Tallarines envueltos con pulpo, calamar y camarones acompañados de pimentón, cebolla y ajonjolí bañados en salsa de anguila.',
+      precio: 44000,
+      ingredientesQuitar: ['Pulpo', 'Calamar', 'Camarones', 'Pimentón', 'Cebolla'],
+      ingredientesAñadir: [
+        { ingrediente: 'Tofu marinado', precio: 6000 },
+        { ingrediente: 'Pollo teriyaki', precio: 6500 },
+        { ingrediente: 'Carne de res salteada', precio: 8000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/StriploinCertifiedAngusBeef .jpg',
+      nombre: 'Striploin Certified Angus Beef',
+      descripcion: 'Certified Angus Beef. Acompañantes papas rústicas o a la francesa.',
+      precio: 90000,
+      ingredientesQuitar: [],
+      ingredientesAñadir: [
+        { ingrediente: 'Chimichurri', precio: 0 },
+        { ingrediente: 'Salsa de tomate', precio: 0 },
+        { ingrediente: 'Espárragos salteados', precio: 6000 },
+        { ingrediente: 'Tomates asados', precio: 5000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/PicanhaCertifiedAngus.jpg',
+      nombre: 'Picanha Certified Angus Beef',
+      descripcion: 'Certified Angus Beef. Acompañantes papas rústicas o a la francesa.',
+      precio: 93000,
+      ingredientesQuitar: [],
+      ingredientesAñadir: [
+        { ingrediente: 'Chimichurri', precio: 0 },
+        { ingrediente: 'Salsa de tomate', precio: 0 },
+        { ingrediente: 'Espárragos salteados', precio: 6000 },
+        { ingrediente: 'Tomates asados', precio: 5000 }
+      ]
+    },
+    {
+      imagen: '../IMAGES/MENUSABORYALMA/PulpoSaborYAlma.jpg',
+      nombre: 'Pulpo Sabor Y Alma',
+      descripcion: 'Pulpo al horno en cocción lenta, encostrado de yuca con parmesano flameado y chimichurri nikkei.',
+      precio: 60000,
+      ingredientesQuitar: ['Parmessano'],
+      ingredientesAñadir: [
+        { ingrediente: 'Panko dorado', precio: 2000 },
+        { ingrediente: 'Tocino crujiente', precio: 3500 }
+      ]
+    }
+  ];
 
-// Función para mostrar los platos en el contenedor
+  // Array de los postres
+  const postres = [
+    {
+        imagen: '../IMAGES/MENUSABORYALMA/CrumbleDeCaramelo.jpg',
+        nombre: 'Crumble de Caramelo',
+        descripcion: 'Trozos de galleta rellena de caramelo horneada y bañada con caramelo caliente, acompañada de fresas y helado de vainilla.',
+        precio: 17000
+    },
+    {
+        imagen: '../IMAGES/MENUSABORYALMA/VolcanDeArequipe.jpg',
+        nombre: 'Volcán de Arequipe',
+        descripcion: 'Suave masa de galleta rellena de arequipe caliente, acompañado de helado de vainilla.',
+        precio: 15500
+    },
+    {
+        imagen: '../IMAGES/MENUSABORYALMA/VolcanDeChocolate.jpg',
+        nombre: 'Volcán de Chocolate',
+        descripcion: 'Torta de chocolate rellena de chocolate caliente, acompañado de helado de vainilla.',
+        precio: 15500
+    },
+    {
+        imagen: '../IMAGES/MENUSABORYALMA/TorreDeCaramelo.jpg',
+        nombre: 'Torre de Caramelo',
+        descripcion: 'Torre de galletas crocantes, helado artesanal de vainilla y caramelo salado, salsa de caramelo y nuez pecana garrapiñada.',
+        precio: 17500
+    },
+    {
+        imagen: '../IMAGES/MENUSABORYALMA/CheescakeFrutosRojos.jpg',
+        nombre: 'Cheesecake Frutos Rojos',
+        descripcion: 'Crema de cheesecake con base de galleta bañada en salsa de frutos rojos.',
+        precio: 13000
+    }
+];
+
+  
+// Función para mostrar las entradas en el contenedor
 function mostrarEntradas() {
     const contenedor = document.getElementById('contenedor-entradas');
     entradas.forEach(entrada => {
@@ -95,5 +253,42 @@ function mostrarEntradas() {
     });
   }
   
-  // Llamar a la función para mostrar las entradas
+  // Función para mostrar los platos fuertes en el contenedor
+  function mostrarPlatosFuertes() {
+    const contenedor = document.getElementById('contenedor-platosFuertes');
+    platosFuertes.forEach(platoFuerte => {
+        const divItem = document.createElement('div');
+        divItem.className = 'item';
+        
+        divItem.innerHTML = `
+            <img src="${platoFuerte.imagen}" alt="${platoFuerte.nombre}">
+            <h2>${platoFuerte.nombre}</h2>
+            <p>${platoFuerte.descripcion}</p>
+            <p>Precio: ${platoFuerte.precio}</p>
+        `;
+        
+        contenedor.appendChild(divItem);
+    });
+  }
+
+  //Función para mostrar las entradas
+  function mostrarPostres() {
+    const contenedor = document.getElementById('contenedor-postres');
+    postres.forEach(postre => {
+        const divItem = document.createElement('div');
+        divItem.className = 'item';
+        
+        divItem.innerHTML = `
+            <img src="${postre.imagen}" alt="${postre.nombre}">
+            <h2>${postre.nombre}</h2>
+            <p>${postre.descripcion}</p>
+            <p>Precio: ${postre.precio}</p>
+        `;
+        
+        contenedor.appendChild(divItem);
+    });
+}
+  // Llamar a las funciones para mostrar los platos
   mostrarEntradas();
+  mostrarPlatosFuertes();
+  mostrarPostres();
